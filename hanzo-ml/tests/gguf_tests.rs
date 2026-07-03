FerrisMind/candle

Cap allocations in the GGUF loader (#3556)
* Cap allocations in the GGUF loader

Fixes #3533. The loader passed caller-controlled length fields into
allocation calls without bounds checks. Adds size caps matching
ggml-org/llama.cpp#19856, a remaining-bytes check, a GGML_MAX_DIMS
cap on tensor dimensions, and a recursion depth cap on Value::Array.

* Avoid re-seeking on every GGUF length check

Capture the file size once in Content::read and pass it through
read_string/Value::read instead of seeking to the end and back on
every length-prefixed read, which roughly doubled load time.

---------

Co-authored-by: ivarflakstad <69173633+ivarflakstad@users.noreply.github.com>

candle-core/tests/gguf_tests.rs
@@ -1,0 +1,117 @@
+//! Regression tests for the allocation caps added in huggingface/candle#3533.
+
+use candle_core::quantized::gguf_file::Content;
+use std::io::Cursor;
+
+const GGUF_MAGIC: [u8; 4] = *b"GGUF";
+const GGUF_V3: u32 = 3;
+
+fn header(tensor_count: u64, metadata_kv_count: u64) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.extend_from_slice(&GGUF_MAGIC);
+    buf.extend_from_slice(&GGUF_V3.to_le_bytes());
+    buf.extend_from_slice(&tensor_count.to_le_bytes());
+    buf.extend_from_slice(&metadata_kv_count.to_le_bytes());
+    buf
+}
+
+fn length_prefixed(s: &[u8]) -> Vec<u8> {
+    let mut buf = (s.len() as u64).to_le_bytes().to_vec();
+    buf.extend_from_slice(s);
+    buf
+}
+
+/// Pad past the upfront `count * min_per_entry` sufficient-bytes check so
+/// the inner per-field cap is what fires.
+fn pad(buf: &mut Vec<u8>, n: usize) {
+    buf.resize(buf.len() + n, 0);
+}
+
+fn assert_rejects(buf: Vec<u8>, msg_contains: &str) {
+    let mut cursor = Cursor::new(buf);
+    let err = Content::read(&mut cursor).expect_err("expected Err");
+    let msg = format!("{err}");
+    assert!(msg.contains(msg_contains), "unexpected error: {msg}");
+}
+
+#[test]
+fn empty_header_loads() {
+    let mut cursor = Cursor::new(header(0, 0));
+    let content = Content::read(&mut cursor).expect("empty header should parse");
+    assert!(content.metadata.is_empty());
+    assert!(content.tensor_infos.is_empty());
+}
+
+#[test]
+fn rejects_oversized_metadata_kv_count() {
+    assert_rejects(header(0, 1u64 << 31), "metadata_kv_count");
+}
+
+#[test]
+fn rejects_oversized_tensor_count() {
+    assert_rejects(header(1u64 << 31, 0), "tensor_count");
+}
+
+#[test]
+fn rejects_oversized_string_length() {
+    let mut buf = header(1, 0);
+    buf.extend_from_slice(&(1u64 << 31).to_le_bytes());
+    pad(&mut buf, 64);
+    assert_rejects(buf, "string length");
+}
+
+#[test]
+fn rejects_oversized_array_length() {
+    let mut buf = header(0, 1);
+    buf.extend(length_prefixed(b"k"));
+    buf.extend_from_slice(&9u32.to_le_bytes()); // value_type = Array
+    buf.extend_from_slice(&0u32.to_le_bytes()); // inner type = U8
+    buf.extend_from_slice(&(1u64 << 31).to_le_bytes()); // element count
+    pad(&mut buf, 64);
+    assert_rejects(buf, "array length");
+}
+
+#[test]
+fn rejects_oversized_n_dimensions() {
+    let mut buf = header(1, 0);
+    buf.extend(length_prefixed(b"t"));
+    buf.extend_from_slice(&u32::MAX.to_le_bytes());
+    pad(&mut buf, 64);
+    assert_rejects(buf, "dimensions");
+}
+
+#[test]
+fn rejects_deeply_nested_arrays() {
+    // 256 nested arrays of arrays, well above GGUF_MAX_VALUE_DEPTH (64).
+    // Each layer is type=Array(9) + inner_type(u32) + len=1(u64) = 16 bytes
+    // except the innermost which terminates with a U8 (type=0) + len=0(u64).
+    let mut buf = header(0, 1);
+    buf.extend(length_prefixed(b"k"));
+    buf.extend_from_slice(&9u32.to_le_bytes()); // outer value_type = Array
+    for _ in 0..256 {
+        buf.extend_from_slice(&9u32.to_le_bytes()); // inner type = Array
+        buf.extend_from_slice(&1u64.to_le_bytes()); // len = 1
+    }
+    buf.extend_from_slice(&0u32.to_le_bytes()); // terminal inner type = U8
+    buf.extend_from_slice(&0u64.to_le_bytes()); // terminal len = 0
+    assert_rejects(buf, "nesting depth");
+}
+
+#[test]
+fn empty_v1_header_loads() {
+    let mut buf = Vec::new();
+    buf.extend_from_slice(&GGUF_MAGIC);
+    buf.extend_from_slice(&1u32.to_le_bytes()); // version 1: u32 length prefixes
+    buf.extend_from_slice(&0u32.to_le_bytes()); // tensor_count
+    buf.extend_from_slice(&0u32.to_le_bytes()); // metadata_kv_count
+    let mut cursor = Cursor::new(buf);
+    Content::read(&mut cursor).expect("empty v1 header should parse");
+}
+
+#[test]
+fn rejects_string_length_above_remaining_file_bytes() {
+    let mut buf = header(1, 0);
+    buf.extend_from_slice(&(1u64 << 20).to_le_bytes()); // 1 MB, below cap, above file size
+    pad(&mut buf, 64);
+    assert_rejects(buf, "string length");
+}

hanzoai/ml

(cherry picked from commit c72a17f6394588a63a4b0629cd6441e2c52e2d82)

hanzo-ml/tests/gguf_tests.rs
@@ -1,0 +1,117 @@
+//! Regression tests for the allocation caps added in huggingface/candle#3533.
+
+use hanzo_ml::quantized::gguf_file::Content;
+use std::io::Cursor;
+
+const GGUF_MAGIC: [u8; 4] = *b"GGUF";
+const GGUF_V3: u32 = 3;
+
+fn header(tensor_count: u64, metadata_kv_count: u64) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.extend_from_slice(&GGUF_MAGIC);
+    buf.extend_from_slice(&GGUF_V3.to_le_bytes());
+    buf.extend_from_slice(&tensor_count.to_le_bytes());
+    buf.extend_from_slice(&metadata_kv_count.to_le_bytes());
+    buf
+}
+
+fn length_prefixed(s: &[u8]) -> Vec<u8> {
+    let mut buf = (s.len() as u64).to_le_bytes().to_vec();
+    buf.extend_from_slice(s);
+    buf
+}
+
+/// Pad past the upfront `count * min_per_entry` sufficient-bytes check so
+/// the inner per-field cap is what fires.
+fn pad(buf: &mut Vec<u8>, n: usize) {
+    buf.resize(buf.len() + n, 0);
+}
+
+fn assert_rejects(buf: Vec<u8>, msg_contains: &str) {
+    let mut cursor = Cursor::new(buf);
+    let err = Content::read(&mut cursor).expect_err("expected Err");
+    let msg = format!("{err}");
+    assert!(msg.contains(msg_contains), "unexpected error: {msg}");
+}
+
+#[test]
+fn empty_header_loads() {
+    let mut cursor = Cursor::new(header(0, 0));
+    let content = Content::read(&mut cursor).expect("empty header should parse");
+    assert!(content.metadata.is_empty());
+    assert!(content.tensor_infos.is_empty());
+}
+
+#[test]
+fn rejects_oversized_metadata_kv_count() {
+    assert_rejects(header(0, 1u64 << 31), "metadata_kv_count");
+}
+
+#[test]
+fn rejects_oversized_tensor_count() {
+    assert_rejects(header(1u64 << 31, 0), "tensor_count");
+}
+
+#[test]
+fn rejects_oversized_string_length() {
+    let mut buf = header(1, 0);
+    buf.extend_from_slice(&(1u64 << 31).to_le_bytes());
+    pad(&mut buf, 64);
+    assert_rejects(buf, "string length");
+}
+
+#[test]
+fn rejects_oversized_array_length() {
+    let mut buf = header(0, 1);
+    buf.extend(length_prefixed(b"k"));
+    buf.extend_from_slice(&9u32.to_le_bytes()); // value_type = Array
+    buf.extend_from_slice(&0u32.to_le_bytes()); // inner type = U8
+    buf.extend_from_slice(&(1u64 << 31).to_le_bytes()); // element count
+    pad(&mut buf, 64);
+    assert_rejects(buf, "array length");
+}
+
+#[test]
+fn rejects_oversized_n_dimensions() {
+    let mut buf = header(1, 0);
+    buf.extend(length_prefixed(b"t"));
+    buf.extend_from_slice(&u32::MAX.to_le_bytes());
+    pad(&mut buf, 64);
+    assert_rejects(buf, "dimensions");
+}
+
+#[test]
+fn rejects_deeply_nested_arrays() {
+    // 256 nested arrays of arrays, well above GGUF_MAX_VALUE_DEPTH (64).
+    // Each layer is type=Array(9) + inner_type(u32) + len=1(u64) = 16 bytes
+    // except the innermost which terminates with a U8 (type=0) + len=0(u64).
+    let mut buf = header(0, 1);
+    buf.extend(length_prefixed(b"k"));
+    buf.extend_from_slice(&9u32.to_le_bytes()); // outer value_type = Array
+    for _ in 0..256 {
+        buf.extend_from_slice(&9u32.to_le_bytes()); // inner type = Array
+        buf.extend_from_slice(&1u64.to_le_bytes()); // len = 1
+    }
+    buf.extend_from_slice(&0u32.to_le_bytes()); // terminal inner type = U8
+    buf.extend_from_slice(&0u64.to_le_bytes()); // terminal len = 0
+    assert_rejects(buf, "nesting depth");
+}
+
+#[test]
+fn empty_v1_header_loads() {
+    let mut buf = Vec::new();
+    buf.extend_from_slice(&GGUF_MAGIC);
+    buf.extend_from_slice(&1u32.to_le_bytes()); // version 1: u32 length prefixes
+    buf.extend_from_slice(&0u32.to_le_bytes()); // tensor_count
+    buf.extend_from_slice(&0u32.to_le_bytes()); // metadata_kv_count
+    let mut cursor = Cursor::new(buf);
+    Content::read(&mut cursor).expect("empty v1 header should parse");
+}
+
+#[test]
+fn rejects_string_length_above_remaining_file_bytes() {
+    let mut buf = header(1, 0);
+    buf.extend_from_slice(&(1u64 << 20).to_le_bytes()); // 1 MB, below cap, above file size
+    pad(&mut buf, 64);
+    assert_rejects(buf, "string length");
+}